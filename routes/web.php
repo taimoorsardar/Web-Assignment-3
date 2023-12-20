3c778dc8759c3cdc65abc336247fab003c095716
Taimoor Sardar <tsardar.bese21seecs@seecs.edu.pk>
2023-12-21 02:29:21 +0500

    Authentication upgraded

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,11 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+/*
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+*/
+Route::get('/home', [HomeController::class,'index'])->middleware('auth')->name('home');
 
+Route::get('post',[HomeController::class, 'post'])->middleware(['auth','admin']);
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
